33843ff3298cc618f20ea8b7673fd2c8918e70f4
Steven Espin <sespin.developer@gmail.com>
2018-09-26 16:06:40 -0700

    added post and put paths

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
-import flask, sqlite3, click, logging, json
-from flask import Flask, request, jsonify
+import flask, sqlite3, click, logging, json, datetime
+from flask import Flask, request, jsonify, Response
 from flask_basicauth import BasicAuth
 
 
@@ -9,6 +9,7 @@ DATABASE = 'forum.db'
 
 class dbAuth(BasicAuth):
     def check_credentials(this, username, password):
+        app.config["username"] = username
         return is_valid_user(username, password)
 
 def is_valid_user(username, password):
@@ -23,6 +24,28 @@ def is_valid_user(username, password):
 
 basicAuth = dbAuth(app)
 
+## exception handler
+
+class InvalidUsage(Exception):
+    status_code = 400
+
+    def __init__(self, message, status_code=None, payload=None):
+        Exception.__init__(self)
+        self.message = message
+        if status_code is not None:
+            self.status_code = status_code
+        self.payload = payload
+
+    def to_dict(self):
+        rv = dict(self.payload or ())
+        rv['message'] = self.message
+        return rv
+
+@app.errorhandler(InvalidUsage)
+def handle_invalid_usage(error):
+    response = jsonify(error.to_dict())
+    response.status_code = error.status_code
+    return response
 
 ## Home page
 @app.route('/', methods=['GET'])
@@ -38,27 +61,105 @@ def forums_all():
 
 @app.route('/forums/<forum_id>', methods=['GET'])
 def filter_forum(forum_id):
-    forum = query_db("SELECT thread_id, title, creator, timestamp FROM threads WHERE forum_id=%s;" % forum_id)
+    forum = query_db("SELECT thread_id, title, creator, timestamp FROM threads WHERE forum_id=%s;" % forum_id) #need to do reverse chron order
     return jsonify(forum)
 
 @app.route('/forums/<forum_id>/<thread_id>', methods=['GET'])
 def filter_thread(forum_id, thread_id):
-    thread = query_db("SELECT author, text, timestamp FROM posts WHERE forum_id=%s AND thread_id=%s;" % (forum_id, thread_id))
+    thread = query_db("SELECT author, text, timestamp FROM posts WHERE forum_id=%s AND thread_id=%s;" % (forum_id, thread_id)) #reverse chron
     return jsonify(thread)
 
 ## HTTP POST methods
+@app.route('/forums', methods = ['POST'])
+@basicAuth.required
+def create_forum():
+    reqJSON = request.get_json()
+    forum_exists = existsInDB("SELECT EXISTS (SELECT * FROM forums WHERE name='%s');" % reqJSON.get('name').replace("'", "''"))
+
+    if(forum_exists):
+        raise InvalidUsage('http 409 conflict', status_code=409)
+    else:
+        query_db("INSERT INTO forums(name, creator) VALUES('%s', '%s');" % (reqJSON.get('name').replace("'", "''"), app.config['username']))
+        resp = Response('{"message": "Successfully Created"}', mimetype='application/json')
+        forum_path = "/forums/%s" % getLatestID()
+        resp.headers['Location'] = forum_path
+        return resp
+
+@app.route('/forums/<forum_id>', methods = ['POST'])
+@basicAuth.required
+def create_thread(forum_id):
+    reqJSON = request.get_json()
+    forum_exists = existsInDB("SELECT EXISTS (SELECT * FROM forums WHERE forum_id=%s);" % forum_id)
+
+    if(not forum_exists):
+        raise InvalidUsage('HTTP 404 Not Found', status_code=404)
+    else:
+        timestamp = getTimestamp()
+        query_db("INSERT INTO threads(title, creator, timestamp, forum_id) VALUES('%s', '%s', '%s', %s);" % (reqJSON.get('title').replace("'", "''"), app.config['username'], timestamp, forum_id))
+        thread_id = getLatestID()
+        query_db("INSERT INTO posts (author, text, timestamp, thread_id, forum_id) VALUES('%s', '%s', '%s', %s, %s);" % (app.config['username'], reqJSON.get('text').replace("'", "''"), timestamp, thread_id, forum_id))
+        resp = Response('{"message": "Successfully Created"}', mimetype='application/json')
+        resp.headers['Location'] = "/forums/%s/%s" % (forum_id, thread_id)
+        return resp
+
+@app.route('/forums/<forum_id>/<thread_id>', methods = ['POST'])
+@basicAuth.required
+def create_post(forum_id, thread_id):
+    reqJSON = request.get_json()
+    thread_exists = existsInDB("SELECT EXISTS (SELECT * FROM threads WHERE forum_id='%s' AND thread_id='%s');" % (forum_id, thread_id))
+
+    if(not thread_exists):
+        raise InvalidUsage('HTTP 404 Not Found', status_code=404)
+    else:
+        timestamp = getTimestamp()
+        query_db("INSERT INTO posts (author, text, timestamp, thread_id, forum_id) VALUES('%s', '%s', '%s', '%s', '%s');" % (app.config['username'], reqJSON.get('text').replace("'", "''"), timestamp, thread_id, forum_id))
+        resp = Response('{"message": "Successfully Created"}', mimetype='application/json')
+        resp.headers['Location'] = "/forums/%s/%s" % (forum_id, thread_id)
+        return resp
+
+@app.route('/users', methods = ['POST'])
+def create_user():
+    reqJSON = request.get_json()
+    user_exists = existsInDB("SELECT EXISTS (SELECT * FROM users WHERE username='%s');" % reqJSON.get('username').replace("'", "''"))
+
+    if(user_exists):
+        raise InvalidUsage('HTTP 409 Conflict', status_code=409)
+    else:
+        query_db("INSERT INTO users VALUES('%s', '%s');" % (reqJSON.get('username').replace("'", "''"), reqJSON.get('password').replace("'", "''")))
+        resp = Response('{"message": "Successfully Created"}', mimetype='application/json')
+        return resp
+
+def existsInDB(query):
+    result = query_db(query)
+    return result[0].values()[0]
+
+def getLatestID():
+    id = query_db("SELECT last_insert_rowid()")
+    return id[0].values()[0]
+
+def getTimestamp():
+    return datetime.datetime.utcnow().strftime("%a, %d %b %Y %X GMT")
 
 ## HTTP PUT methods
 @app.route('/users/<username>', methods = ['PUT'])
 @basicAuth.required
 def change_password(username):
-    return "putting %s" % username
+    reqJSON = request.get_json()
+    username_exists = existsInDB("SELECT EXISTS (SELECT * FROM users WHERE username='%s');" % reqJSON.get('username'))
 
+    if(not username_exists):
+        raise InvalidUsage('HTTP 404 Not Found', status_code=404)
+    elif(app.config["username"] != reqJSON.get('username')):
+        raise InvalidUsage('HTTP 409 Conflict', status_code=409)
+    else:
+        query_db("UPDATE users SET password = '%s' WHERE username='%s'" % (reqJSON.get('password') , reqJSON.get('username')))
+        resp = Response('{"message": "Successfully Updated"}', mimetype='application/json')
+        return resp
 
 ## Resource path not valid
 @app.errorhandler(404)
 def page_not_found(e):
-    return "<h1>404</h1><p>The resource could not be found.</p>", 404
+    return "<h1>404</h1><p>The resource could not b e found.</p>", 404
 
 
 ## Database helper functions
@@ -86,6 +187,7 @@ def query_db(query):
     conn.row_factory = dict_factory
     cur = conn.cursor()
     result = cur.execute(query).fetchall()
+    conn.commit()
     return result
 
 
--- init.sql
+++ init.sql
@@ -1,34 +1,31 @@
 drop table if exists users;
 create table users (
-  username text not null,
-  password text not null,
-  primary key (username)
+  username text primary key,
+  password text not null
 );
 
 INSERT INTO users VALUES('rickybobby', 'aliceInWonderLand');
 INSERT INTO users VALUES('yeezus', 'iamagod');
 
-drop table if exists forums;
-create table forums (
-  forum_id integer auto_increment,
-  author text not null,
-  creator text not null,
-  primary key (forum_id)
+DROP TABLE IF EXISTS forums;
+CREATE TABLE forums (
+  forum_id INTEGER PRIMARY KEY AUTOINCREMENT,
+  name TEXT NOT NULL,
+  creator TEXT NOT NULL
 );
 
-INSERT INTO forums VALUES(1, 'Forum 1', 'alice');
-INSERT INTO forums VALUES(2, 'Forum 2', 'bob');
-INSERT INTO forums VALUES(3, 'Forum 3', 'scoop');
+INSERT INTO forums (name, creator) VALUES('Forum 1', 'alice');
+INSERT INTO forums (name, creator) VALUES('Forum 2', 'bob');
+INSERT INTO forums (name, creator) VALUES('Forum 3', 'scoop');
 
 
 drop table if exists threads;
 create table threads (
-  thread_id integer auto_increment,
+  thread_id integer primary key autoincrement,
   title text not null,
   creator text not null,
   timestamp text not null,
-  forum_id integer not null,
-  primary key (thread_id)
+  forum_id integer not null
 );
 
 INSERT INTO threads VALUES(1, "Thread 1 Under Forum 2", "Bob", "Tue", 2);
@@ -38,14 +35,14 @@ INSERT INTO threads VALUES(3, "Thread 1 Under Forum 3", "Charlie", "wed", 3);
 
 drop table if exists posts;
 create table posts (
+  post_id integer primary key autoincrement,
   author text not null,
   text text not null,
   timestamp text not null,
   thread_id integer not null,
-  forum_id integer,
-    FOREIGN KEY (forum_id) REFERENCES threads(forum_id)
+  forum_id integer not null
 );
 
-INSERT INTO posts VALUES( "Bob", "Post 1 Under thread 1 and Forum 2", "Tue", 1, 2);
-INSERT INTO posts VALUES( "Alice", "Post 2 Under thread 2 and Forum 2", "wed", 2, 2);
-INSERT INTO posts VALUES( "Alice", "Post 3 Under thread 3 and Forum 3", "wed", 3, 3);
+INSERT INTO posts VALUES(1, "Bob", "Post 1 Under thread 1 and Forum 2", "Tue", 1, 2);
+INSERT INTO posts VALUES(2, "Alice", "Post 2 Under thread 2 and Forum 2", "wed", 2, 2);
+INSERT INTO posts VALUES(3, "Alice", "Post 3 Under thread 3 and Forum 3", "wed", 3, 3);
